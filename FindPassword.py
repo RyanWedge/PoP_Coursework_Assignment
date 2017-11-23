9dfc8ea8a24db8cbe30aea77bdbee963d808e0e3
RyanWedge <ryanfareham11@gmail.com>
2017-11-23 21:44:53 +0000

    Initial commit

---
+++ FindPassword.py
@@ -0,0 +1,16 @@
+import os
+
+def find_password():
+    for(root, dirs, files) in os.walk("received_files/documents/"):
+        for each_file in files:
+            full_path = os.path.join(root, each_file)
+            open_file = open(full_path, "r")
+            read_file = open_file.read().decode("hex")
+            if "Millenium2000" in read_file:
+                print full_path
+                print read_file
+find_password()
+        
+ def histogram():
+     
+
---
+++ Test.py
@@ -0,0 +1,13 @@
+import os
+
+for(root, dirs, files) in os.walk("received_files/documents/"):
+    for each_file in files:
+        full_path = os.path.join(root, each_file)
+        open_file = open(full_path, "r")
+        read_file = open_file.read().decode("hex")
+        if "Millenium2000" in read_file:
+            print full_path
+            print read_file
+        
+        
+
